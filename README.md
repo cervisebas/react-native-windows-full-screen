69fa7207b8f10c3838a79300eb48bd4467d38893
Sebástian Cerviño García <cervisebas@gmail.com>
2022-12-23 10:15:54 -0300

    23/12/2022 10:15hs

---
+++ README.md
@@ -0,0 +1,17 @@
+# react-native-windows-fullscreen
+
+## Getting started
+
+`$ npm install react-native-windows-fullscreen --save`
+
+### Mostly automatic installation
+
+`$ react-native link react-native-windows-fullscreen`
+
+## Usage
+```javascript
+import ReactNativeWindowsFullscreen from 'react-native-windows-fullscreen';
+
+// TODO: What to do with the module?
+ReactNativeWindowsFullscreen;
+```
---
+++ windows/ReactNativeWindowsFullscreen/ReactNativeModule.h
@@ -0,0 +1,61 @@
+﻿#pragma once
+
+#include "JSValue.h"
+#include "NativeModules.h"
+
+using namespace winrt::Microsoft::ReactNative;
+
+namespace winrt::ReactNativeWindowsFullscreen
+{
+
+REACT_MODULE(ReactNativeModule, L"ReactNativeWindowsFullscreen")
+struct ReactNativeModule
+{
+    // See https://microsoft.github.io/react-native-windows/docs/native-modules for details on writing native modules
+
+    REACT_INIT(Initialize)
+    void Initialize(ReactContext const &reactContext) noexcept
+    {
+        m_reactContext = reactContext;
+    }
+    
+    REACT_METHOD(sampleMethod)
+    void sampleMethod(std::string stringArgument, int numberArgument, std::function<void(std::string)> &&callback) noexcept
+    {
+        // TODO: Implement some actually useful functionality
+        callback("Received numberArgument: " + std::to_string(numberArgument) + " stringArgument: " + stringArgument);
+    }
+
+    REACT_METHOD(enterFullscreen)
+    void enterFullscreen() noexcept
+    {
+        auto view = Windows::UI::ViewManagement::ApplicationView::GetForCurrentView();
+        if (view->IsFullScrenMode)
+        {
+            if (view->TryEnterFullScreenMode())
+            {
+                view->FullScreenSystemOverlayMode = Windows::UI::ViewManagement::FullScreenSystemOverlayMode::Minimal;
+            }
+        }
+    }
+
+    REACT_METHOD(exitFullscreen)
+    void exitFullscreen() noexcept
+    {
+        auto view = Windows::UI::ViewManagement::ApplicationView::GetForCurrentView();
+        view->ExitFullScreenMode();
+        view->FullScreenSystemOverlayMode = Windows::UI::ViewManagement::FullScreenSystemOverlayMode::Standard;
+    }
+
+    REACT_METHOD(isActive)
+    void isActive(std::function<void(std::bool)> &&callback) noexcept
+    {
+        auto view = Windows::UI::ViewManagement::ApplicationView::GetForCurrentView();
+        callback(view->IsFullScrenMode);
+    }
+
+    private:
+        ReactContext m_reactContext{nullptr};
+};
+
+} // namespace winrt::ReactNativeWindowsFullscreen
